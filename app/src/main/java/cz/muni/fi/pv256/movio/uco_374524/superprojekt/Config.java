f26f4d0bb602ef7f823136719d24d485eacf655b
Stanislav Nedbálek <stanislav.nedbalek@gmail.com>
2015-10-10 23:32:39 +0200

    Styles + app icon + Added gridview

---
+++ app/src/main/java/cz/muni/fi/pv256/movio/uco_374524/superprojekt/Config.java
@@ -0,0 +1,8 @@
+package cz.muni.fi.pv256.movio.uco_374524.superprojekt;
+
+/**
+ * Created by prasniatko on 21/09/15.
+ */
+public class Config {
+    public static final boolean STRICT_MODE_ENABLED = BuildConfig.DEBUG;
+}
--- app/src/main/java/cz/muni/fi/pv256/movio/uco_374524/superprojekt/activity/MainActivity.java
+++ app/src/main/java/cz/muni/fi/pv256/movio/uco_374524/superprojekt/activity/MainActivity.java
@@ -1,39 +1,50 @@
 package cz.muni.fi.pv256.movio.uco_374524.superprojekt.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import cz.muni.fi.pv256.movio.uco_374524.superprojekt.R;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-  }
+    private static final String TAG = ".MainActivity";
 
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_main, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG,
+            "onCreate() called with: " + "savedInstanceState = [" + savedInstanceState + "]");
     }
 
-    return super.onOptionsItemSelected(item);
-  }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called with: " + "");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
